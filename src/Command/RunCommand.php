e816d2d5a71823134fb466a5515be0b36a90034d
Tang Rufus <tangrufus@gmail.com>
2017-03-19 13:36:45 +0800

    Configure RunCommand

--- src/Command/RunCommand.php
+++ src/Command/RunCommand.php
@@ -12,7 +12,14 @@ class RunCommand extends BaseCommand
 {
     protected function configure()
     {
-        $this->setName('imposter:run');
+        $this->setName('imposter:run')
+            ->setAliases(['imposter'])
+            ->setDescription("Transform all required packages' namespaces into yours")
+            ->setHelp(<<<EOT
+The <info>imposter:run</info> command goes through all required packages' autoload-ed files, and
+prefixes all namespaces with yours, as defined as <info>extra.imposter.namespace</info> in composer.json
+EOT
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
--- tests/functional/ComposerListCept.php
+++ tests/functional/ComposerListCept.php
@@ -4,4 +4,5 @@ $I = new FunctionalTester($scenario);
 $I->wantTo('see all imposter commands in $composer list');
 
 $I->runShellCommand('composer list');
-$I->canSeeInShellOutput('imposter:run');
+$I->seeInShellOutput('imposter:run');
+$I->seeInShellOutput('Transform all required packages\' namespaces into yours');
