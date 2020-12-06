228ce5debf1f728ff8ade2714383d185c2197f0b
TangRufus <tangrufus@gmail.com>
2020-12-06 23:31:59 +0000

    Warn when skipping non-existing `autoload` paths

--- tests/functional/ComposerUpdateCept.php
+++ tests/functional/ComposerUpdateCept.php
@@ -19,4 +19,6 @@ $I->seeInThisFile('MyPlugin\\\\Vendor\\\\Dummy\\\\Psr4\\\\DummyOne');
 $I->seeInThisFile('MyPlugin\\\\Vendor\\\\Dummy\\\\Psr4\\\\DummyTwo');
 $I->seeInThisFile('MyPlugin\\\\Vendor\\\\Dummy\\\\Psr4\\\\Sub\\\\DummyOne');
 
-$I->seeInShellOutput(PHP_EOL . 'Success: Imposter transformed vendor files.' . PHP_EOL . PHP_EOL);
+// TODO: Codeception cannot see stdErr output.
+//$I->seeInShellOutput(PHP_EOL . 'Warning: Imposter failed to transformed 2 of the autoload path(s).' . PHP_EOL);
+$I->seeInShellOutput(PHP_EOL . 'Success: Imposter transformed vendor files.' . PHP_EOL);
